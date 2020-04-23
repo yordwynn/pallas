a5c8acafb22867f8291e909942475aafd3253928
Yury Egorov <yurij.a.egorov@gmail.com>
2020-04-23 22:39:48 +0500

    Get last data in Russia from HJU

--- src/main/scala/Covid19/Sources/Source.scala
+++ src/main/scala/Covid19/Sources/Source.scala
@@ -1,24 +1,51 @@
 package Covid19.Sources
 
 import Covid19.Protocol.Summary
+import sttp.client._
+import sttp.client.asynchttpclient.WebSocketHandler
+import sttp.client.{SttpBackend, basicRequest}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 sealed trait Source {
   def baseUrl: String
-  def getSummaryByCountry(country: String)(implicit context: ExecutionContext): Future[Summary]
+  def getSummaryByCountry(country: String)(implicit context: ExecutionContext, backend: SttpBackend[Future, Nothing, WebSocketHandler]): Future[Summary]
 }
 
-sealed class Jhu extends Source {
+final class Jhu extends Source {
   override val baseUrl: String = "https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/csse_covid_19_data/csse_covid_19_time_series/"
 
-  override def getSummaryByCountry(country: String)(implicit context: ExecutionContext): Future[Summary] = {
-    Future(Summary("", 0, 0, 0))
+  override def getSummaryByCountry(country: String)(implicit context: ExecutionContext, backend: SttpBackend[Future, Nothing, WebSocketHandler]): Future[Summary] = {
+    val confirmed = getSummaryByCountryByCategory(country, "confirmed")
+    val deaths = getSummaryByCountryByCategory(country, "deaths")
+    val recovered = getSummaryByCountryByCategory(country, "recovered")
+
+    Await.result(confirmed zip deaths zip recovered, Duration.Inf)
+
+    confirmed.flatMap(conf => deaths.flatMap(rec => recovered.map(death => Summary(country, conf, rec, death))))
+  }
+
+  private def getSummaryByCountryByCategory(country: String, category: String)(implicit context: ExecutionContext, backend: SttpBackend[Future, Nothing, WebSocketHandler]): Future[Int] = {
+    val request = basicRequest.get(uri"${baseUrl}time_series_covid19_${category}_global.csv")
+    request.send().map(_.body).map {
+      case Left(_) => 0
+      case Right(s) => extract(s, country)
+    }
+  }
+
+  private def extract(data: String, country: String): Int = {
+    Option(data.split("\n").toList).
+      map {
+        l1 => l1.map(_.split(",").toList).
+          filter(l2 => l2(1) == country).
+          map(_.last.toInt).sum
+      }.getOrElse(0)
   }
 }
 
-sealed class CovidApi extends Source {
+final class CovidApi extends Source {
   override val baseUrl: String = "https://api.covid19api.com/"
 
-  override def getSummaryByCountry(country: String)(implicit context: ExecutionContext): Future[Summary] = ???
+  override def getSummaryByCountry(country: String)(implicit context: ExecutionContext, backend: SttpBackend[Future, Nothing, WebSocketHandler]): Future[Summary] = ???
 }
--- src/main/scala/Pallas.scala
+++ src/main/scala/Pallas.scala
@@ -1,3 +1,5 @@
+import Covid19.Sources.Jhu
+
 import sttp.client._
 import sttp.client.asynchttpclient.WebSocketHandler
 import sttp.client.asynchttpclient.future.AsyncHttpClientFutureBackend
@@ -11,22 +13,8 @@ object Pallas {
     implicit val sttpBackend: SttpBackend[Future, Nothing, WebSocketHandler] = AsyncHttpClientFutureBackend()
 
     val country: String = "Russia"
-    val request = basicRequest.get(
-      uri"https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-    )
-    val response = request.send().map(_.body).map {
-      case Left(_) => 0
-      case Right(s) => parse(s, country)
-    }.map(print(_))
-    Await.result(response, Duration.Inf)
-  }
 
-  def parse(data: String, country: String): Int = {
-    Option(data.split("\n").toList).
-      map {
-        l1 => l1.map(s => s.split(",").toList).
-          filter(l2 => l2(1) == country).
-          map(_.last.toInt).sum
-      }.getOrElse(0)
+    val confirmed = new Jhu().getSummaryByCountry(country).map(print(_))
+    Await.result(confirmed, Duration.Inf)
   }
 }
