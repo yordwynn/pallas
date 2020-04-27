44e829df902330de9e5ab4399b88d21a663f7875
Yury Egorov <yurij.a.egorov@gmail.com>
2020-04-28 00:12:11 +0500

    Return Summary in JavaWrapper

--- src/main/scala/Covid19/JavaWrapper.scala
+++ src/main/scala/Covid19/JavaWrapper.scala
@@ -1,6 +1,6 @@
 package Covid19
 
-import Covid19.Protocol.{Response, Summary}
+import Covid19.Protocol.Summary
 import Covid19.Sources.Jhu
 import sttp.client.SttpBackend
 import sttp.client.asynchttpclient.WebSocketHandler
@@ -14,7 +14,7 @@ class JavaWrapper {
   implicit val sttpBackend: SttpBackend[Future, Nothing, WebSocketHandler] = AsyncHttpClientFutureBackend()
   val source = new Jhu()
 
-  def getSummaryByCountry(countryCode: String): java.util.concurrent.CompletionStage[Response] = {
-    source.getSummaryByCountry(countryCode).asJava
+  def getSummaryByCountry(countryCode: String): java.util.concurrent.CompletionStage[Summary] = {
+    source.getSummaryByCountry(countryCode).map(x => x.asInstanceOf[Summary]).asJava
   }
 }
